51fdaf2c759a0fc8814942ab983b4dded36136f9
rapidev-1 <rapidev.softwares@gmail.com>
2023-03-26 13:19:55 +0500

    CHANGES

--- routes/messagebird.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const messageBirdController = require('../controllers/messagebird');
-
-const router = express.Router();
-router.post("/sendMessage", messageBirdController.SendMessage);
-router.get("/verifyMessage", messageBirdController.OnVerifyMessage);
-
-exports.routes = router;
