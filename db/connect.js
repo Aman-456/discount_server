b2c8d8a6b26d17b95ace7c79aeafc4289bad2f90
rapidev-1 <rapidev.softwares@gmail.com>
2023-03-27 19:43:35 +0500

    master

--- db/connect.js
+++ db/connect.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 
 require("dotenv").config();
-// const db_url = process.env.MONGO_DB;
 const db_url = process.env.DATABASE_URL;
-// const db_url = process.env.DATABASE_URL_PROD;
 
 mongoose
   .connect(db_url, {
